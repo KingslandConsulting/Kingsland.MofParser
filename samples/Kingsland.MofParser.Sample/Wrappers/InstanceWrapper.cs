5ecce897294fd45984d581cbabc892c834a91e69
Michael Clayton <mike@kingslandconsulting.co.uk>
2015-09-15 20:30:36 +0100

    Moved Kingsland.MofParser.Sample to root "samples" folder.

---
+++ samples/Kingsland.MofParser.Sample/Wrappers/InstanceWrapper.cs
@@ -0,0 +1,165 @@
+﻿using Kingsland.MofParser.Objects;
+using System;
+
+namespace Kingsland.MofParser.Sample.Wrappers
+{
+
+    public class InstanceWrapper
+    {
+
+        #region Constructors
+
+        protected InstanceWrapper(string filename, string computerName, Instance instance)
+        {
+            this.Filename = filename;
+            this.ComputerName = computerName;
+            this.Instance = instance;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Filename
+        {
+            get;
+            private set;
+        }
+
+        public string ComputerName
+        {
+            get;
+            private set;
+        }
+
+        public Instance Instance
+        {
+            get;
+            private set;
+        }
+
+        public string ResourceId
+        {
+            get
+            {
+                return this.GetStringProperty("ResourceID");
+            }
+        }
+
+        public string ClassName
+        {
+            get
+            {
+                return this.Instance.ClassName;
+            }
+        }
+
+        public string ResourceType
+        {
+            get
+            {
+                // ResourceID = "[ResourceType]ResourceName"
+                return InstanceWrapper.GetResourceTypeFromResourceId(this.ResourceId);
+            }
+        }
+
+        public string ResourceName
+        {
+            get
+            {
+                // ResourceID = "[ResourceType]ResourceName"
+                return InstanceWrapper.GetResourceNameFromResourceId(this.ResourceId);
+            }
+        }
+
+        public string[] DependsOn
+        {
+            get
+            {
+                return (string[])this.Instance.Properties["ResourceID"];
+            }
+        }
+
+        public string ModuleName
+        {
+            get
+            {
+                return this.GetStringProperty("ModuleName");
+            }
+        }
+
+        public string ModuleVersion
+        {
+            get
+            {
+                return this.GetStringProperty("ModuleVersion");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static InstanceWrapper FromInstance(string filename, string computerName, Instance instance)
+        {
+            switch (instance.ClassName)
+            {
+                case "HDLE_xAcl":
+                case "HDLE_xHostsEntry":
+                case "HDLE_xServiceResource":
+                case "HDLE_xVolume":
+                case "MSFT_ArchiveResource":
+                case "MSFT_Credential":
+                case "MSFT_DSCMetaConfiguration":
+                case "MSFT_EnvironmentResource":
+                case "MSFT_GroupResource":
+                case "MSFT_FileDirectoryConfiguration":
+                case "MSFT_PackageResource":
+                case "MSFT_RoleResource":
+                case "MSFT_RegistryResource":
+                case "MSFT_UserResource":
+                case "OMI_ConfigurationDocument":
+                    return new InstanceWrapper(filename, computerName, instance);
+                case "MSFT_ScriptResource":
+                    return new ScriptResourceWrapper(filename, computerName, instance);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static string GetResourceTypeFromResourceId(string resourceId)
+        {
+            // ResourceID = "[ResourceType]ResourceName"
+            if (string.IsNullOrEmpty(resourceId)) { return null; }
+            return resourceId.Split(']')[0].Substring(1);
+        }
+
+        private static string GetResourceNameFromResourceId(string resourceId)
+        {
+            // ResourceID = "[ResourceType]ResourceName"
+            if (string.IsNullOrEmpty(resourceId)) { return null; }
+            return resourceId.Split(']')[1];
+        }
+
+        protected static string GetStringProperty(Instance instance, string propertyName)
+        {
+            if (instance.Properties.ContainsKey(propertyName))
+            {
+                return instance.Properties[propertyName].ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected string GetStringProperty(string propertyName)
+        {
+            return InstanceWrapper.GetStringProperty(this.Instance, propertyName);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ samples/Kingsland.MofParser.Sample/Wrappers/InstanceWrapperGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Kingsland.MofParser.Sample.Wrappers
+{
+
+    public sealed class InstanceWrapperGroup
+    {
+
+        public string Filename
+        {
+            get;
+            set;
+        }
+
+        public string ComputerName
+        {
+            get;
+            set;
+        }
+
+        public List<InstanceWrapper> Wrappers
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
