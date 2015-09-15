09a178e5d832c2d686a6467688be1fa8a0df95ee
Michael Clayton <mike@kingslandconsulting.co.uk>
2015-09-15 20:31:14 +0100

    Added new Kingsland.MofParser.HtmlReport sample program.

---
+++ samples/Kingsland.MofParser.HtmlReport/Program.cs
@@ -0,0 +1,45 @@
+﻿using Kingsland.MofParser.HtmlReport.Wrappers;
+using RazorEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Kingsland.MofParser.Sample
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // get the list of mof filenames
+            var mofRoot = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            mofRoot = Path.GetDirectoryName(mofRoot);
+            mofRoot = Path.GetDirectoryName(mofRoot);
+            mofRoot = Path.GetDirectoryName(mofRoot);
+            mofRoot = Path.Combine(mofRoot, "dsc");
+            var filenames = Directory.GetFiles(mofRoot, "*.mof", SearchOption.AllDirectories);
+
+            // parse the mof files
+            var wrappers = new List<InstanceWrapper>();
+            foreach (var filename in filenames)
+            {
+                var instances = PowerShellDscHelper.ParseMofFileInstances(filename);
+                wrappers.AddRange(
+                        instances.Select(instance => InstanceWrapper.FromInstance(
+                            Path.GetFileName(Path.GetDirectoryName(filename)),
+                            Path.GetFileNameWithoutExtension(filename),
+                            instance))
+                );
+            }
+
+            string template = File.ReadAllText("MofFileSummary.cshtml");
+            string result = Razor.Parse(template, wrappers);
+            File.WriteAllText("summary.htm", result);
+
+        }
+
+    }
+
+}
---
+++ samples/Kingsland.MofParser.HtmlReport/Wrappers/ScriptResourceWrapper.cs
@@ -0,0 +1,40 @@
+﻿using Kingsland.MofParser.Objects;
+
+namespace Kingsland.MofParser.HtmlReport.Wrappers
+{
+
+    public sealed class ScriptResourceWrapper : InstanceWrapper
+    {
+
+        public ScriptResourceWrapper(string filename, string computerName, Instance instance)
+            : base(filename, computerName, instance)
+        {
+        }
+
+        public string GetScript
+        {
+            get
+            {
+                return InstanceWrapper.GetStringProperty(this.Instance, "GetScript");
+            }
+        }
+
+        public string TestScript
+        {
+            get
+            {
+                return InstanceWrapper.GetStringProperty(this.Instance, "TestScript");
+            }
+        }
+
+        public string SetScript
+        {
+            get
+            {
+                return InstanceWrapper.GetStringProperty(this.Instance, "SetScript");
+            }
+        }
+
+    }
+
+}
