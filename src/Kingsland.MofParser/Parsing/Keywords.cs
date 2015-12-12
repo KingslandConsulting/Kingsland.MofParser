28525d4b32b31d5cab34287b31c949aa32a97350
Michael Clayton <mike@kingslandconsulting.co.uk>
2015-12-12 18:45:53 +0000

    Grouped Keywords literals by location in spec document

--- src/Kingsland.MofParser/Parsing/Keywords.cs
+++ src/Kingsland.MofParser/Parsing/Keywords.cs
@@ -10,63 +10,71 @@
     public static class Keywords
     {
 
+        // 7.1 Compiler directives
         public const string PRAGMA = "#pragma";
-
-        public const string ANY = "any";
-        public const string AS = "as";
-        public const string ASSOCIATION = "association";
-
-        public const string BOOLEAN = "boolean";
-
-        public const string CLASS = "class";
-
-        public const string DATETIME = "datetime";
-        public const string DISABLEOVERRIDE = "disableoverride";
-
-        public const string ENABLEOVERRIDE = "enableoverride";
-        public const string ENUMERATION = "enumeration";
-        public const string ENUMERATIONVALUE = "enumerationvalue";
-
-        public const string FALSE = "false";
-        public const string FLAVOR = "flavor";
-
         public const string INCLUDE = "include";
-        public const string INSTANCE = "instance";
 
-        public const string METHOD = "method";
-
-        public const string NULL = "null";
-
-        public const string OCTECTSTRING = "octetstring";
-        public const string OF = "of";
-
-        public const string PARAMETER = "parameter";
+        // 7.2 Qualifiers
+        public const string SCOPE = "scope";
+        public const string ANY = "any";
+        public const string POLICY = "policy";
+        public const string ENABLEOVERRIDE = "enableoverride";
+        public const string DISABLEOVERRIDE = "disableoverride";
+        public const string RESTRICTED = "restricted";
+        public const string ENUMERATIONVALUE = "enumerationvalue";
         public const string PROPERTY = "property";
+        public const string REFPROPERTY = "reference";
+        public const string METHOD = "method";
+        public const string PARAMETER = "parameter";
+        public const string QUALIFIERTYPE = "qualifiertype";
 
+        public const string FLAVOR = "flavor";
         public const string QUALIFIER = "qualifier";
 
-        public const string REAL32 = "real32";
-        public const string REAL64 = "real64";
-        public const string REF = "ref";
-        public const string RESTRICTED = "restricted";
+        // 7.3.1 Enumeration declaration
+        public const string ENUMERATION = "enumeration";
 
-        public const string SCOPERESTRICTED = "scope";
-        public const string SINT8 = "sint8";
-        public const string SINT16 = "sint16";
-        public const string SINT32 = "sint32";
-        public const string SINT64 = "sint64";
-        public const string STRING = "string";
+        // 7.3.2 Structure declaration
         public const string STRUCTURE = "structure";
 
+        // 7.3.3 Class declaration
+        public const string CLASS = "class";
+        public const string VOID = "void";
+
+        // 7.3.4 Association declaration
+        public const string ASSOCIATION = "association";
+
+        // 7.3.5 Primitive types declarations
+        public const string DT_UINT8 = "uint8";
+        public const string DT_UINT16 = "uint16";
+        public const string DT_UINT32 = "uint32";
+        public const string DT_UINT64 = "uint64";
+        public const string DT_SINT8 = "sint8";
+        public const string DT_SINT16 = "sint16";
+        public const string DT_SINT32 = "sint32";
+        public const string DT_SINT64 = "sint64";
+        public const string DT_REAL32 = "real32";
+        public const string DT_REAL64 = "real64";
+        public const string DT_STRING = "string";
+        public const string DT_DATETIME = "datetime";
+        public const string DT_BOOLEAN = "boolean";
+        public const string DT_OCTECTSTRING = "octetstring";
+
+        // 7.3.6 Reference type declaration
+        public const string REF = "ref";
+
+        // A.1 Value definitions
+        public const string INSTANCE = "instance";
+        public const string VALUE = "value";
+        public const string AS = "as";
+        public const string OF = "of";
+
+        // A.17.6 Boolean value
+        public const string FALSE = "false";
         public const string TRUE = "true";
 
-        public const string UINT8 = "uint8";
-        public const string UINT16 = "uint16";
-        public const string UINT32 = "uint32";
-        public const string UINT64 = "uint64";
-
-        public const string VALUE = "value";
-        public const string VOID = "void";
+        // A.17.7 Null value
+        public const string NULL = "null";
 
     }
 
