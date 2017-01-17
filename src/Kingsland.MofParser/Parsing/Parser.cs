2da789d061791d662a59caef61bb11f273620bf9
Michael Clayton <mike@kingslandconsulting.co.uk>
2017-01-17 23:42:53 +0000

    Added wrappers for this.CurrentState.Peek/Read* in Parser.cs

--- src/Kingsland.MofParser/Parsing/Parser.cs
+++ src/Kingsland.MofParser/Parsing/Parser.cs
@@ -41,14 +41,14 @@ namespace Kingsland.MofParser.Parsing
             }
         }
 
-        private ParserState Descend()
+        internal ParserState Descend()
         {
             var newState = (ParserState)this.CurrentState.Clone();
             this.States.Push(newState);
             return newState;
         }
 
-        private void Commit()
+        internal void Commit()
         {
             if (this.States.Count <= 1)
             {
@@ -60,13 +60,70 @@ namespace Kingsland.MofParser.Parsing
             this.States.Push(currentState);
         }
 
-        private void Backtrack()
+        internal void Backtrack()
         {
             if (this.States.Count <= 1)
             {
                 throw new InvalidOperationException("Cannot roll back past initial state.");
             }
             this.States.Pop();
+        }
+
+        #endregion
+
+        #region CurrentState Members
+
+        internal bool Eof
+        {
+            get
+            {
+                return this.CurrentState.Eof;
+            }
+        }
+
+        internal Token Peek()
+        {
+            return this.CurrentState.Peek();
+        }
+
+        public T Peek<T>() where T : Token
+        {
+            return this.CurrentState.Peek<T>();
+        }
+
+        internal IdentifierToken PeekIdentifier()
+        {
+            return this.CurrentState.PeekIdentifier();
+        }
+
+        internal bool PeekIdentifier(string name)
+        {
+            return this.CurrentState.PeekIdentifier(name);
+        }
+
+        internal Token Read()
+        {
+            return this.Read();
+        }
+
+        internal bool TryRead<T>(ref T result) where T : Token
+        {
+            return this.CurrentState.TryRead<T>(ref result);
+        }
+
+        public T Read<T>() where T : Token
+        {
+            return this.CurrentState.Read<T>();
+        }
+
+        internal IdentifierToken ReadIdentifier()
+        {
+            return this.CurrentState.ReadIdentifier();
+        }
+
+        internal IdentifierToken ReadIdentifier(string name)
+        {
+            return this.CurrentState.ReadIdentifier(name);
         }
 
         #endregion
