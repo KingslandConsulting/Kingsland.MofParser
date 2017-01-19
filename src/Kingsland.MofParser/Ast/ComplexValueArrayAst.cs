83e1602f0b7ef22cf6d689c1bb805461c971676e
Michael Clayton <Michael.Clayton@investec.co.uk>
2017-01-19 09:04:31 +0000

    Converted ComplexValueArrayAst to TryParse

--- src/Kingsland.MofParser/Ast/ComplexValueArrayAst.cs
+++ src/Kingsland.MofParser/Ast/ComplexValueArrayAst.cs
@@ -52,24 +52,57 @@ namespace Kingsland.MofParser.Ast
         ///     complexValueArray = "{" [ complexValue *( "," complexValue) ] "}"
         ///
         /// </remarks>
+        internal static bool TryParse(Parser parser, ref ComplexValueArrayAst node, bool throwIfError = false)
+        {
+
+            // "{"
+            var blockOpen = default(BlockOpenToken);
+            if (!parser.TryRead(ref blockOpen))
+            {
+                return AstNode.HandleUnexpectedToken(parser.Peek(), throwIfError);
+            }
+
+            // [ complexValue *( "," complexValue) ]
+            var complexValues = new List<ComplexValueAst>();
+            while (!parser.Eof && (parser.Peek<BlockCloseToken>() == null))
+            {
+                var comma = default(CommaToken);
+                if ((complexValues.Count > 0) && (!parser.TryRead(ref comma)))
+                {
+                    return AstNode.HandleUnexpectedToken(parser.Peek(), throwIfError);
+                }
+                var complexValue = default(ComplexValueAst);
+                if (ComplexValueAst.TryParse(parser, ref complexValue, throwIfError))
+                {
+                    complexValues.Add(complexValue);
+                }
+                else
+                {
+                    return AstNode.HandleUnexpectedToken(parser.Peek(), throwIfError);
+                }
+            }
+
+            // "}"
+            var blockClose = default(BlockCloseToken);
+            if (!parser.TryRead(ref blockClose))
+            {
+                return AstNode.HandleUnexpectedToken(parser.Peek(), throwIfError);
+            }
+
+            // build the result
+            var result = new ComplexValueArrayAst();
+            result.Values.AddRange(complexValues);
+
+            // return the result
+            node = result;
+            return true;
+
+        }
+
         internal new static ComplexValueArrayAst Parse(Parser parser)
         {
-            var state = parser.CurrentState;
-            // complexValueArray =
-            var node = new ComplexValueArrayAst();
-            // "{"
-            state.Read<BlockOpenToken>();
-            // [ complexValue
-            node.Values.Add(ComplexValueAst.Parse(parser));
-            // *( "," complexValue) ]
-            while (state.Peek<CommaToken>() != null)
-            {
-                state.Read<CommaToken>();
-                node.Values.Add(ComplexValueAst.Parse(parser));
-            }
-            // "}"
-            state.Read<BlockCloseToken>();
-            // return the result
+            var node = default(ComplexValueArrayAst);
+            ComplexValueArrayAst.TryParse(parser, ref node, true);
             return node;
         }
 
