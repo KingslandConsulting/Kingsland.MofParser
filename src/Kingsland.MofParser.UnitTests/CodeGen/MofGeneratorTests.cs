67e25763d901013bd7d6cb34f9991e1ba70a8ecb
Vic Peters <vicp@iqmetrix.com>
2018-12-10 16:18:11 -0600

    Added a test to isolate an infinite recursion

---
+++ src/Kingsland.MofParser.UnitTests/CodeGen/MofGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Kingsland.MofParser.Ast;
+using Kingsland.MofParser.CodeGen;
+using NUnit.Framework;
+
+namespace Kingsland.MofParser.UnitTests.CodeGen
+{
+    public class MofGeneratorTests
+    {
+        [Test]
+        public void ConvertingAPropertyNodeShouldNotCauseStackOverflow()
+        {
+            PropertyValueAst node = (PropertyValueAst) Activator.CreateInstance(typeof(ReferenceValueArrayAst), true);
+
+            MofGenerator.ConvertToMof(node);
+        }
+    }
+}
