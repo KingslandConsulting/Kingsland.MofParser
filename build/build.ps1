087e9cff480b4c9fac8a498377d71a9adce54eeb
mikeclayton <mike@kingslandconsulting.co.uk>
2019-03-10 20:37:17 +0000

    More build logging

--- build/build.ps1
+++ build/build.ps1
@@ -117,6 +117,16 @@ catch
 }
 
 
+# echo Newtonsoft.Json binding log
+$logPath = [System.IO.Path]::Combine($fusiondir, "Default\MSBuild.exe\Newtonsoft.Json, Version=9.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed.HTM");
+$logText = [System.IO.File]::ReadAllText($logPath);
+write-host "**********";
+write-host "fusion log";
+write-host "**********";
+write-host $logText
+write-host "**********";
+
+
 # disable fusion binding
 Set-DotNetFusionBinding -EnableLog        $false `
                         -ForceLog         $false `
